a1a16b03cee1e6538bff38390104aae21867b470
jnboat <jnboat@khu.ac.kr>
2019-06-28 18:13:54 +0900

    still on working

--- CNN_test_18_1/conv_layer.v
+++ CNN_test_18_1/conv_layer.v
@@ -1,6 +1,7 @@
 module conv_layer(
 	input clk,
 	input RESET,
+	input de,
 	input in,
 	input start_wr, start_rd,
 	output result
@@ -8,7 +9,7 @@ module conv_layer(
 
 parameter bit_depth = 16;
 
-
+wire de;
 wire [bit_depth*9-1:0] in;
 wire [bit_depth-1:0] result;
 reg [bit_depth*14-1:0] w1, w2; 
@@ -20,7 +21,86 @@ wire [bit_depth-1:0] data_b = 16'd1;
 reg wren_a, wren_b, rden_a, rden_b;
 
 
+/////////////////////////////////////////////////////////////////////////////////////
 
+always@(negedge clk)
+begin
+	if(RESET)
+	begin
+		cnt_wr <= 10'd0;
+		cnt_rd <= 10'd0;
+		addr_a <= 5'd31;
+		addr_b <= 5'd31;
+		wren_a <= 1'b0;
+		wren_b <= 1'b0;
+		rden_a <= 1'b0;
+		rden_b <= 1'b0;
+	end
+	
+
+	else
+	begin
+		cnt_wr<=10'd0;
+		cnt_rd<=10'd0;
+		wren_a <= 1'b0;
+		wren_b <= 1'b0;
+		addr_a <= 5'd31;
+		addr_b  <= 5'd31;
+	end
+end
+
+
+
+
+wire [bit_depth-1:0] in0_q_a, in0_q_b;
+bram bram_in0 (
+	.clock	(in0_clk),
+	.wren_a	(in0_wren_a),
+	.wren_b	(in0_wren_b),
+	.data_a	(in0_data_a),
+	.data_b	(in0_data_b),
+	.address_a(in0_addr_a),
+	.address_b(in0_addr_b),
+	.rden_a	(in0_rden_a),
+	.rden_b	(in0_rden_b),
+	
+	.q_a		(in0_q_a),
+	.q_b		(in0_q_b)
+);
+
+wire [bit_depth-1:0] in1_q_a, in1_q_b;
+bram bram_in1 (
+	.clock	(in1_clk),
+	.wren_a	(in1_wren_a),
+	.wren_b	(in1_wren_b),
+	.data_a	(in1_data_a),
+	.data_b	(in1_data_b),
+	.address_a(in1_addr_a),
+	.address_b(in1_addr_b),
+	.rden_a	(in1_rden_a),
+	.rden_b	(in1_rden_b),
+	
+	.q_a		(in1_q_a),
+	.q_b		(in1_q_b)
+);
+
+wire [bit_depth-1:0] in2_q_a, in2_q_b;
+bram bram_in2 (
+	.clock	(in2_clk),
+	.wren_a	(in2_wren_a),
+	.wren_b	(in2_wren_b),
+	.data_a	(in2_data_a),
+	.data_b	(in2_data_b),
+	.address_a(in2_addr_a),
+	.address_b(in2_addr_b),
+	.rden_a	(in2_rden_a),
+	.rden_b	(in2_rden_b),
+	
+	.q_a		(in2_q_a),
+	.q_b		(in2_q_b)
+);
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 always@(negedge clk)
 begin
@@ -107,7 +187,7 @@ end
 
 
 wire [bit_depth-1:0] q_a, q_b;
-bram bram0 (
+bram bram_w (
 	.clock	(clk),
 	.wren_a	(wren_a),
 	.wren_b	(wren_b),
@@ -210,155 +290,5 @@ paral_add p_add0(
 	.result(result_a)
 );
 
-<<<<<<< HEAD
-cnn conv3(
-	.in0(in[15:0]),
-	.in1(in[31:16]),
-	.in2(in[47:32]),
-	.in3(in[63:48]),
-	.in4(in[79:64]),
-	.in5(in[95:80]),
-	.in6(in[111:96]),
-	.in7(in[127:112]),
-	.in8(in[143:128]),
-	.w0(w[15:0]),
-	.w1(w[31:16]),
-	.w2(w[47:32]),
-	.w3(w[63:48]),
-	.w4(w[79:64]),
-	.w5(w[95:80]),
-	.w6(w[111:96]),
-	.w7(w[127:112]),
-	.w8(w[143:128]),
-	.result(result3)
-);
-
-cnn conv4(
-	.in0(in[159:144]),
-	.in1(in[175:160]),
-	.in2(in[191:176]),
-	.in3(in[207:192]),
-	.in4(in[223:208]),
-	.in5(in[239:224]),
-	.in6(in[255:240]),
-	.in7(in[271:256]),
-	.in8(in[287:272]),
-	.w0(w[159:144]),
-	.w1(w[175:160]),
-	.w2(w[191:176]),
-	.w3(w[207:192]),
-	.w4(w[223:208]),
-	.w5(w[239:224]),
-	.w6(w[255:240]),
-	.w7(w[271:256]),
-	.w8(w[287:272]),
-	.result(result4)
-);
-
-cnn conv5(
-	.in0(in[303:288]),
-	.in1(in[319:304]),
-	.in2(in[335:320]),
-	.in3(in[351:336]),
-	.in4(in[367:352]),
-	.in5(in[383:368]),
-	.in6(in[399:384]),
-	.in7(in[415:400]),
-	.in8(in[431:416]),
-	.w0(w[303:288]),
-	.w1(w[319:304]),
-	.w2(w[335:320]),
-	.w3(w[351:336]),
-	.w4(w[367:352]),
-	.w5(w[383:368]),
-	.w6(w[399:384]),
-	.w7(w[415:400]),
-	.w8(w[431:416]),
-	.result(result5)
-);
-
-paral_add p_add1(
-	.data0x(result3),
-	.data1x(result4),
-	.data2x(result5),
-	.result(result_b)
-);
-cnn conv6(
-	.in0(in[15:0]),
-	.in1(in[31:16]),
-	.in2(in[47:32]),
-	.in3(in[63:48]),
-	.in4(in[79:64]),
-	.in5(in[95:80]),
-	.in6(in[111:96]),
-	.in7(in[127:112]),
-	.in8(in[143:128]),
-	.w0(w[15:0]),
-	.w1(w[31:16]),
-	.w2(w[47:32]),
-	.w3(w[63:48]),
-	.w4(w[79:64]),
-	.w5(w[95:80]),
-	.w6(w[111:96]),
-	.w7(w[127:112]),
-	.w8(w[143:128]),
-	.result(result6)
-);
-
-cnn conv7(
-	.in0(in[159:144]),
-	.in1(in[175:160]),
-	.in2(in[191:176]),
-	.in3(in[207:192]),
-	.in4(in[223:208]),
-	.in5(in[239:224]),
-	.in6(in[255:240]),
-	.in7(in[271:256]),
-	.in8(in[287:272]),
-	.w0(w[159:144]),
-	.w1(w[175:160]),
-	.w2(w[191:176]),
-	.w3(w[207:192]),
-	.w4(w[223:208]),
-	.w5(w[239:224]),
-	.w6(w[255:240]),
-	.w7(w[271:256]),
-	.w8(w[287:272]),
-	.result(result7)
-);
-
-cnn conv8(
-	.in0(in[303:288]),
-	.in1(in[319:304]),
-	.in2(in[335:320]),
-	.in3(in[351:336]),
-	.in4(in[367:352]),
-	.in5(in[383:368]),
-	.in6(in[399:384]),
-	.in7(in[415:400]),
-	.in8(in[431:416]),
-	.w0(w[303:288]),
-	.w1(w[319:304]),
-	.w2(w[335:320]),
-	.w3(w[351:336]),
-	.w4(w[367:352]),
-	.w5(w[383:368]),
-	.w6(w[399:384]),
-	.w7(w[415:400]),
-	.w8(w[431:416]),
-	.result(result8)
-);
-
-paral_add p_add2(
-	.data0x(result6),
-	.data1x(result7),
-	.data2x(result8),
-	.result(result_c)
-);
-
-assign result = result_a + result_b + result_c;
-=======
-
 assign result = result_a;
->>>>>>> 189a62c202e19c582985203686f074e94f11a252
 endmodule
