1b7fb8880c15258f19530bf8fc6b41ffc47065f4
sjsong08 <tenoclo@khu.ac.kr>
2019-07-16 14:14:50 +0900

    pulled

--- emif_test/issp/synth/issp.v
+++ emif_test/issp/synth/issp.v
@@ -5,20 +5,20 @@
 `timescale 1 ps / 1 ps
 module issp (
 		output wire [2:0]   source, // sources.source
-		input  wire [324:0] probe   //  probes.probe
+		input  wire [351:0] probe   //  probes.probe
 	);
 
 	altsource_probe_top #(
 		.sld_auto_instance_index ("YES"),
 		.sld_instance_index      (0),
 		.instance_id             ("NONE"),
-		.probe_width             (325),
+		.probe_width             (352),
 		.source_width            (3),
 		.source_initial_value    ("0"),
 		.enable_metastability    ("NO")
 	) in_system_sources_probes_0 (
 		.source     (source), //  output,    width = 3, sources.source
-		.probe      (probe),  //   input,  width = 325,  probes.probe
+		.probe      (probe),  //   input,  width = 352,  probes.probe
 		.source_ena (1'b1)    // (terminated),                       
 	);
 
--- emif_test/stp02/stp02_inst.v
+++ emif_test/stp02/stp02_inst.v
@@ -1,5 +1,5 @@
-	stp u0 (
-		.acq_data_in    (_connected_to_acq_data_in_),    //   input,  width = 80,     tap.acq_data_in
+	stp02 u0 (
+		.acq_data_in    (_connected_to_acq_data_in_),    //   input,  width = 10,     tap.acq_data_in
 		.acq_trigger_in (_connected_to_acq_trigger_in_), //   input,   width = 3,        .acq_trigger_in
 		.acq_clk        (_connected_to_acq_clk_)         //   input,   width = 1, acq_clk.clk
 	);
